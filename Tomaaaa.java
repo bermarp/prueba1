f70ac69cee9cede6a85a52a9a3eb6b4fc21ec074
Bernat Marti Penades <bernip_21@hotmail.com>
2012-12-11 12:22:50 +0100

    asdf

---
+++ Tomaaaa.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Tomaaaa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tomaaaa
+{
+    public static void main(String[] args){
+        System.out.println("Ves como merezco un 10??");
+    }
+}
